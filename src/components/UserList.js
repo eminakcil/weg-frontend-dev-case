6869ab2e07285ef1459aa44a3230b1ff64197ba1
eminakcil <eminakcil@outlook.com>
2022-06-28 21:00:20 +0300

    sorting users by vote

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,11 +4,15 @@ import UserItem from './UserItem'
 export default function UserList() {
   const { users } = useSelector((state) => state.main)
 
+  const sortedUsers = users
+    ? [...users].sort((a, b) => (b?.vote || 0) - (a?.vote || 0))
+    : false
+
   return (
     <>
-      {users && (
+      {sortedUsers && (
         <ul>
-          {users.map((user) => (
+          {sortedUsers.map((user) => (
             <UserItem
               user={user}
               key={user.login.uuid}
