b2500e919917ee996839b41d2cf4cdf0def2cbfe
eminakcil <eminakcil@outlook.com>
2022-06-29 01:09:07 +0300

    animation library

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux'
 import UserItem from './UserItem'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export default function UserList() {
   const { users } = useSelector((state) => state.main)
@@ -8,10 +9,18 @@ export default function UserList() {
     ? [...users].sort((a, b) => (b?.vote || 0) - (a?.vote || 0))
     : false
 
+  const [animationParent] = useAutoAnimate({
+    duration: 800,
+    easing: 'ease-out',
+  })
+
   return (
     <>
       {sortedUsers && (
-        <div className="user-list">
+        <div
+          className="user-list"
+          ref={animationParent}
+        >
           {sortedUsers.map((user, index) => (
             <UserItem
               user={user}
