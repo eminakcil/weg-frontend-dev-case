3a8c437018b324c4a467daf10971086f5bf6226a
eminakcil <eminakcil@outlook.com>
2022-06-29 21:46:22 +0300

    404 page style

--- src/pages/errors/NotFound.js
+++ src/pages/errors/NotFound.js
@@ -1,12 +1,25 @@
 import { Helmet } from 'react-helmet'
+import { useNavigate } from 'react-router-dom'
 
 export default function NotFound() {
+  const navigate = useNavigate()
+
   return (
     <>
       <Helmet>
-        <title>404 - Not found</title>
+        <title>404 - Sayfa Bulunamadı</title>
       </Helmet>
-      <div>Page not found!</div>
+      <div className="error-page">
+        <div className="error-code">404</div>
+        <div className="error-message">Sayfa Bulunamadı</div>
+        <button
+          onClick={() => {
+            navigate('/')
+          }}
+        >
+          Ana Sayfa
+        </button>
+      </div>
     </>
   )
 }
